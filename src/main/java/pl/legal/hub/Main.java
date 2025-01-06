a81b1c2666a9a47a3941d71fcb6161ca1920234a
kubaeclipse <jakub.jankowski@eclipsegroup.co.uk>
2025-01-06 13:59:17 +0100

    First commit

--- src/main/java/pl/legal/hub/Main.java
+++ src/main/java/pl/legal/hub/Main.java
@@ -1,4 +1,4 @@
-package pl.javastart.courses;
+package pl.legal.hub;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
